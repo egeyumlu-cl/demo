a8cfb874a0e565b37412d9bfef07e2f9e7ceca8f
Ege Yumlu <ege.yumlu@rockfeather.com>
2024-04-11 19:41:06 +0000

    changes to the wrong view

--- models/views/customer_acquisition_monthly.sql
+++ models/views/customer_acquisition_monthly.sql
@@ -1,6 +1,13 @@
 SELECT
-    DATE_TRUNC('month', MIN(o.ORDER_DATE)) AS acquisition_month,
+    DATE_TRUNC('month', first_order_date) AS acquisition_month,
     COUNT(DISTINCT c.ID) AS new_customers
-  FROM {{ source('jaffle_shop', 'Customers') }} c
-  JOIN {{ source('jaffle_shop', 'Orders') }} o ON c.ID = o.USER_ID
-  GROUP BY DATE_TRUNC('month', MIN(o.ORDER_DATE))
+FROM
+     {{ source('jaffle_shop', 'Customers') }} c
+JOIN
+    (SELECT
+        USER_ID,
+        MIN(ORDER_DATE) AS first_order_date
+     FROM  {{ source('jaffle_shop', 'Orders') }} o
+     GROUP BY USER_ID) o ON c.ID = o.USER_ID
+GROUP BY
+    DATE_TRUNC('month', first_order_date);
